be3de52db0551a07972fa0f0a0efdc5af5d04409
raaihansvg <rlazuardi163@gmail.com>
2026-05-21 10:58:43 +0700

    pertemuan11

---
+++ PERTEMUAN 11/dolar.py
@@ -0,0 +1,15 @@
+def solve_knapsack(n, w, berat, values):
+    dp = [0] * (w + 1)
+
+    for i in range(n):
+        for j in range(berat[i], w + 1):
+            dp[j] = max(dp[j], dp[j - berat[i]] + values[i])
+
+    return dp[w]
+
+
+n, w = map(int, input().split())
+berat = list(map(int, input().split()))
+values = list(map(int, input().split()))
+
+print(solve_knapsack(n, w, berat, values))
